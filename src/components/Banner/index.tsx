e63d818d75d0bb514d45b3c1c0ad67570df27dd5
Mateus Alcantara <mateuscastro.al@gmail.com>
2021-08-05 07:29:58 -0300

    Add travel types

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,39 @@
+import { Flex, Heading, Text, Image } from "@chakra-ui/react";
+import React from "react";
+
+export const Banner = () => {
+  return (
+    <Flex
+      h={["163px", "335px"]}
+      bg="url(/background.png)"
+      bgSize="cover"
+      align="center"
+      justify="space-between"
+    >
+      <Flex
+        align="center"
+        justify="space-between"
+        w="100%"
+        maxW="1120px"
+        m="0 auto"
+      >
+        <Flex maxW="26rem" direction="column" p={["16px", "0px"]}>
+          <Heading color="gray.50" fontSize={["20px", "36px"]}>
+            5 Continentes, <br />
+            infinitas possibilidades.
+          </Heading>
+          <Text
+            mt={["0.5rem", "1.5rem"]}
+            color="gray.50"
+            fontSize={["14px", "20px"]}
+          >
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+          </Text>
+        </Flex>
+        <Flex display={["none", "flex"]} mt="150px">
+          <Image src="/airplane.svg" alt="Airplane image" />
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
 import Head from "next/head";
 
-import { Text, Flex, Heading, Image } from "@chakra-ui/react";
-
 import { Header } from "../components/Header";
+import React from "react";
+import { Banner } from "../components/Banner";
+import { Flex, Image, useBreakpointValue } from "@chakra-ui/react";
 
 export default function Home() {
+  const imageSrc = useBreakpointValue({
+    sm: "/travel_types_web.png",
+    base: "/travel_types_mobile.png",
+  });
+
   return (
     <div>
       <Head>
@@ -13,37 +19,9 @@ export default function Home() {
 
       <main>
         <Header />
-        <Flex
-          h={["163px", "335px"]}
-          bg="url(/background.png)"
-          bgSize="cover"
-          align="center"
-          justify="space-between"
-        >
-          <Flex
-            align="center"
-            justify="space-between"
-            w="100%"
-            maxW="1120px"
-            m="0 auto"
-          >
-            <Flex maxW="26rem" direction="column" p={["16px", "0px"]}>
-              <Heading color="gray.50" fontSize={["20px", "36px"]}>
-                5 Continentes, <br />
-                infinitas possibilidades.
-              </Heading>
-              <Text
-                mt={["0.5rem", "1.5rem"]}
-                color="gray.50"
-                fontSize={["14px", "20px"]}
-              >
-                Chegou a hora de tirar do papel a viagem que você sempre sonhou.{" "}
-              </Text>
-            </Flex>
-            <Flex display={["none", "flex"]} mt="150px">
-              <Image src="/airplane.svg" alt="Airplane image" />
-            </Flex>
-          </Flex>
+        <Banner />
+        <Flex align="center" justify="center" mt={["36px", "100px"]}>
+          <Image src={imageSrc} alt="travel types" />
         </Flex>
       </main>
     </div>
